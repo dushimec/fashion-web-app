cadb28da3de5b9677770767af8d8fbf55f6aa5f4
kevine00 <kevineumubyeyi9@gmail.com>
2024-04-30 02:17:09 -0700

    update

--- frontend/src/component/shop/Shop.jsx
+++ frontend/src/component/shop/Shop.jsx
@@ -8,6 +8,10 @@ import promdress from '../../assets/promdress.jpg'
 import wedding from '../../assets/wedding.jpg'
 import whitesuit from '../../assets/whitesuit.jpg'
 
+import { FaHeart } from "react-icons/fa";
+import { IoMdHeartEmpty } from "react-icons/io";
+
+
 const Shop = () => {
     return ( 
         <section className="shop">
@@ -28,19 +32,29 @@ const Shop = () => {
 
 
 <div className="body"> 
-<img src={man} alt="" />
+
+<img id="man" src={man} alt="" />
+<button className="btn">SHOP NOW</button>
  
  <div>
 <div className="row1">
-    <div>  <img src={promdress}  alt="" sizes="2%" /> <p>Promdress</p> <p>100,000rwf</p></div>
-    <div id="skirt"> <img src={blackskirt} alt="" /> <p>skirts</p> <p>40,000rwf</p></div>
-    <div><img src={whitesuit} alt="" /><p>Whitesuit</p> <p>120,000rwf</p></div>
+    <div>  <img src={promdress}  alt="" sizes="2%" /> <p>Promdress</p> <p>100,000rwf</p>
+    <FaHeart className="hearticon" color="red" /> <span className="new">NEW</span></div>
+
+    <div id="skirt"> <img src={blackskirt} alt="" /> <p>skirts</p> <p>40,000rwf</p>
+    <IoMdHeartEmpty className="hearticon"color="red"/><span className="sold">SOLD</span></div>
+
+    <div><img id="suit" src={whitesuit} alt="" /><p>Whitesuit</p> <p>120,000rwf</p></div>
 </div>
 
 <div className="row2">
     <div id="baby"> <img src={baby} alt="" /><p>Baby dress</p> <p>80,000rwf</p></div>
-    <div id="wedding"> <img src={wedding}alt="" /><p>wedding dress</p><p>200,000rwf</p></div>
-    <div id="couple"><img src={couple} alt="" /><p>Couple complete</p> <p>80,000rwf</p></div>
+
+    <div id="wedding"> <img src={wedding}alt="" /><p>wedding dress</p><p>200,000rwf</p>
+    <FaHeart className="heart" color="red" /><span className="neww">NEW</span></div>
+
+    <div id="couple"><img src={couple} alt="" /><p>Couple complete</p> <p>80,000rwf</p>
+    <IoMdHeartEmpty className="heart" color="red" /></div>
 </div>
 </div>
 </div>
